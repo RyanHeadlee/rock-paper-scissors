c546715aa8cc2e81d7941c67b24cae5e53ef01fd
Ryan Headlee <headleeryan@gmail.com>
2022-05-08 14:14:40 -0700

    Add UI for choosing rock, paper,or scissors
    Added buttons and new functions. Added a reset button as well.

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
-const button = document.querySelector(".buttonPlay");
-const para1 = document.querySelector(".gameInfo");
-const para2 = document.querySelector(".roundInfo");
+const button = document.querySelector("button");
+const para = document.createElement("p");
+const resultsContainer = document.querySelector(".resultsContainer");
+const playerResults = document.querySelector(".gameLives");
+const computerResults = document.querySelector(".computerLives");
+const reset = document.querySelector(".reset");
 let computerSelection;
 let playerSelection;
-let roundResults;
 let gameLives = 3;
 let computerLives = 3;
 let keepGoing = true;
@@ -24,57 +26,85 @@ function computerPlay() {
 
 // This function plays one round of rock, paper, scissors and determines a winner for the round.
 
-function oneRound(playerSelection, computerSelection) {
-if ((playerSelection == "rock" && computerSelection == "scissors") || (playerSelection == "scissors" && computerSelection == "paper") 
-      || (playerSelection == "paper" && computerSelection == "rock")) {
-    return roundResults = "win";
-  } else if ((playerSelection == "rock" && computerSelection == "paper") || (playerSelection == "scissors" && computerSelection == "rock") 
-      || (playerSelection == "paper" && computerSelection == "scissors")) {
-    return roundResults = "lose";
-  } else if ((playerSelection == "rock"  && computerSelection == "rock") || (playerSelection == "scissors" && computerSelection == "scissors") 
-      || (playerSelection == "paper" && computerSelection == "paper")) {
-    return roundResults = "tied";
-  } else if (playerSelection == null ||playerSelection == "cancel" || playerSelection == "abort") {
-    return roundResults = "cancel";
-  } else {
-    alert("Insert either: Rock, Paper, or Scissors");
-    return roundResults = "tied";
+function playRound(playerSelection, computerSelection) {
+if ((playerSelection == "rock" && computerSelection == "scissors") ||
+      (playerSelection == "scissors" && computerSelection == "paper") ||
+      (playerSelection == "paper" && computerSelection == "rock")) {
+    resultsContainer.appendChild(para);
+    para.textContent = `You win the round; ${playerSelection} beats ${computerSelection}.`;
+    computerLives--;
+    computerResults.textContent = `Computer Lives: ${computerLives}`;
+  } else if ((playerSelection == "rock" && computerSelection == "paper") || 
+        (playerSelection == "scissors" && computerSelection == "rock") || 
+        (playerSelection == "paper" && computerSelection == "scissors")) {
+    resultsContainer.appendChild(para);
+    para.textContent = `You lose the round; ${computerSelection} beats ${playerSelection}.`;
+    gameLives--;
+    playerResults.textContent = `Your Lives: ${gameLives}`;
+  } else if ((playerSelection == "rock"  && computerSelection == "rock") || 
+        (playerSelection == "scissors" && computerSelection == "scissors") || 
+        (playerSelection == "paper" && computerSelection == "paper")) {
+    resultsContainer.appendChild(para);
+    para.textContent = `You tied the round; ${playerSelection} ties ${computerSelection}.`;
   }
 }
 
-// This function will keep playing the round until either the player or the computer no longer has lives.
+// This function checks if your lives are above 0.
 
-function game() {
-  while (keepGoing == true) {
-    if (gameLives > 0 && computerLives > 0) {
-      playerSelection = prompt("Rock, paper, or scissors: " + "").toLowerCase();
-      computerPlay();
-      oneRound(playerSelection, computerSelection);
-      if (roundResults == "win") {
-        console.log(roundResults);
-        computerLives--;
-      } else if (roundResults == "lose") {
-        console.log(roundResults);
-        gameLives--;
-      } else if (roundResults == "tied") {
-        console.log(roundResults);
-      } else if (roundResults == "cancel") {
-        keepGoing = false;
-      }
-    } else if (gameLives == 0) {
-      para1.textContent = `You lose! The computer has ${computerLives} live(s) remaining! So close!`;
-      keepGoing = false;
-    } else {
-      para1.textContent = `You Win! You had ${gameLives} live(s) remaining!`;
-      keepGoing = false;
-    }
+function checkLives () {
+  if (gameLives == 0) {
+    para.textContent = "You Lose!";
+    para.classList.add("loser");
+    return keepGoing = false;
+  } else if (computerLives == 0) {
+    para.textContent = "You Win!"
+    para.classList.add("winner");
+    return keepGoing = false;
   }
+
+  return keepGoing = true;
 }
 
-button.addEventListener("click", function() {
-  keepGoing = true;
+// These event listeners play the round with your selected choice when button is clicked. 
+
+const rock = document.querySelector('.rock');
+rock.addEventListener('click', () => {
+  checkLives();
+  if (keepGoing == true) { 
+    playerSelection = "rock";
+    computerPlay();
+    playRound(playerSelection, computerSelection);
+    checkLives();
+  }
+});
+const paper = document.querySelector('.paper');
+paper.addEventListener('click', () => {
+  checkLives();
+  if (keepGoing == true) {
+    playerSelection = "paper"
+    computerPlay();
+    playRound(playerSelection, computerSelection);
+    checkLives();
+  }
+});
+const scissors = document.querySelector('.scissors');
+scissors.addEventListener('click', () => {
+  checkLives();
+  if (keepGoing == true) {
+    playerSelection = "scissors";
+    computerPlay();
+    playRound(playerSelection, computerSelection);
+    checkLives();
+  }
+});
+
+// Resets the game.
+
+reset.addEventListener('click', () => {
+  para.classList.remove('winner', 'loser');
   gameLives = 3;
   computerLives = 3;
-  para1.textContent = "";
-  game();
+  computerResults.textContent = "Your Lives: 3";
+  playerResults.textContent = "Your Lives: 3";
+  para.textContent = "";
 });
